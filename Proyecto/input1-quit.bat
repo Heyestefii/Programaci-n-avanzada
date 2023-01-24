0b7ff186767e8c8c542615cdfbc05c2602740ae8
Heyestefii <94660008+Heyestefii@users.noreply.github.com>
2023-01-23 23:42:48 -0600

    Delete Proyecto directory

--- Proyecto/input1-quit.bat
+++
@@ -1,6 +0,0 @@
-ls
-/bin/ls
-ls -l
-ls -l ; cat file1
-ls -l ; cat file1 ; grep de file2
-quit
--- Proyecto/input2-quit.bat
+++
@@ -1,6 +0,0 @@
-; cat file1 ; grep foo file2
-cat file1 ; ; grep foo file2
-cat file1 ; ls -l ;
-cat file1 ; ; ; ; ls -l
-; ; ls -l
-quit ; 
